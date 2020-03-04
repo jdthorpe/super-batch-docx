676a0ea9d3e5a316199c8ce0f6a9698aeda5bc5b
Jason Thorpe <jdthorpe@gmail.com>
2020-03-03 22:35:57 -0800

    Updates

---
+++ precache-manifest.c5f4b3056ae7ac5b7e3493d7920471b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86f422ec7800c72a30ddc3718705eea1",
+    "url": "/super-batch/index.html"
+  },
+  {
+    "revision": "fe364291336260f45bc2",
+    "url": "/super-batch/static/css/2.66a375a2.chunk.css"
+  },
+  {
+    "revision": "bde707af3237cbaaa937",
+    "url": "/super-batch/static/css/main.ef317bf7.chunk.css"
+  },
+  {
+    "revision": "fe364291336260f45bc2",
+    "url": "/super-batch/static/js/2.2fa4f0e5.chunk.js"
+  },
+  {
+    "revision": "d28ff64ff4908c0da313274d9125c297",
+    "url": "/super-batch/static/js/2.2fa4f0e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bde707af3237cbaaa937",
+    "url": "/super-batch/static/js/main.5452b211.chunk.js"
+  },
+  {
+    "revision": "c9c106849fa925eeb231",
+    "url": "/super-batch/static/js/runtime-main.40ade205.js"
+  }
+]);
