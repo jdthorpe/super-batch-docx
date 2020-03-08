a4bc651d10909d01a590e87a7e6902ad60bbdc51
Jason Thorpe <jdthorpe@gmail.com>
2020-03-08 00:34:54 -0800

    Updates

---
+++ precache-manifest.df9d1cf25d325b650fc65bb6ea7cf115.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5258d0e1dd215a8e61276f908134a5c3",
+    "url": "/super-batch-docs/index.html"
+  },
+  {
+    "revision": "950df09b217b85a7832b",
+    "url": "/super-batch-docs/static/css/2.4be11a11.chunk.css"
+  },
+  {
+    "revision": "6211fa698699806ef2ce",
+    "url": "/super-batch-docs/static/css/main.ef317bf7.chunk.css"
+  },
+  {
+    "revision": "950df09b217b85a7832b",
+    "url": "/super-batch-docs/static/js/2.40fecd0e.chunk.js"
+  },
+  {
+    "revision": "d28ff64ff4908c0da313274d9125c297",
+    "url": "/super-batch-docs/static/js/2.40fecd0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6211fa698699806ef2ce",
+    "url": "/super-batch-docs/static/js/main.33880d26.chunk.js"
+  },
+  {
+    "revision": "cd6d249201f05630656c",
+    "url": "/super-batch-docs/static/js/runtime-main.47ec9086.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/super-batch-docs/precache-manifest.a22f6bfb2db7c5324efcebbbd36f9aef.js"
+  "/super-batch-docs/precache-manifest.df9d1cf25d325b650fc65bb6ea7cf115.js"
 );
 
 self.addEventListener('message', (event) => {
