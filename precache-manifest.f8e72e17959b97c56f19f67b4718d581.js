32b09a11f655c07f0412d38b8a5bba12ac76b02c
Jason Thorpe <jathorpe@gmail.com>
2024-04-03 15:53:50 -0700

    updates

---
+++ precache-manifest.f8e72e17959b97c56f19f67b4718d581.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50675b10ec7d9c5c39e01bc00acd4f3f",
+    "url": "/super-batch-docs/index.html"
+  },
+  {
+    "revision": "755da3bdb7aadbee0466",
+    "url": "/super-batch-docs/static/css/2.4be11a11.chunk.css"
+  },
+  {
+    "revision": "cce7d5dbb8c00eb0a5eb",
+    "url": "/super-batch-docs/static/css/main.ef317bf7.chunk.css"
+  },
+  {
+    "revision": "755da3bdb7aadbee0466",
+    "url": "/super-batch-docs/static/js/2.8c203479.chunk.js"
+  },
+  {
+    "revision": "d28ff64ff4908c0da313274d9125c297",
+    "url": "/super-batch-docs/static/js/2.8c203479.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cce7d5dbb8c00eb0a5eb",
+    "url": "/super-batch-docs/static/js/main.ea0746f5.chunk.js"
+  },
+  {
+    "revision": "cd6d249201f05630656c",
+    "url": "/super-batch-docs/static/js/runtime-main.47ec9086.js"
+  }
+]);
